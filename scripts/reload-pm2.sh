5c022c44989ece7342693643ca5b5c0b10e6ae4f
Khuzama Shahid <skhuzamashahid@gmail.com>
2021-04-29 11:51:22 +0500

    updated script

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd ~/polkalokr-migration-server/dist
+echo "Starting server screen..."
 screen -S server -d -m node main.js
--- scripts/terminate.sh
+++ scripts/terminate.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+echo "Terminating server screen"
 screen -S server -X quit
+result=$?
+if [ $result -eq 0 ]; then
+echo "Terminated server screen successfully"
+else
+echo "Screen was not terminated because: $result"
+fi
+echo "Copying environment in dist..."
+cp /home/ubuntu/environment/.env /home/ubuntu/polkalokr-migration-server/dist/
+echo "Exiting..."
+exit
