b6631a0acea13b989bd732194b46612a4fa3e5d7
Ahsan <ahsan.ahmedffs2@gmail.com>
2021-08-05 18:03:04 +0500

    blocknumber issues fixed

--- src/functions/getPastEvents.ts
+++ src/functions/getPastEvents.ts
@@ -17,79 +17,100 @@ import {
 } from '../utils/constant';
 
 export const getPastEvents = async (bridge) => {
-  var allEvents = [];
-  var ethNewBlock;
-  var bnbNewBlock;
-  var matNewBlock;
+  let allEvents = [];
+  let ethNewBlock;
+  let bnbNewBlock;
+  let matNewBlock;
 
   if (bridge) {
     if (ERC_BRIDGE && TOKEN_ERC) {
-      var web3 = new Web3(ETH_URL);
-      var contract = new web3.eth.Contract(BRIDGE_ABI, ERC_BRIDGE);
-      await contract.getPastEvents(
-        'Payback',
-        {
-          fromBlock: bridge.ethBlock,
-          toBlock: 'latest',
-        },
-        async (err, events) => {
-          if (events && events.length != 0) {
-            events.forEach((event) => {
-              event.sourceChain = ETH_NETWORK;
-            });
-            allEvents = allEvents.concat(events);
-            ethNewBlock = parseInt(events[0].blockNumber) + 1;
-          }
-        },
-      );
+      let web3 = new Web3(ETH_URL);
+
+      if (bridge.ethBlock) {
+        let contract = new web3.eth.Contract(BRIDGE_ABI, ERC_BRIDGE);
+        await contract.getPastEvents(
+          'Payback',
+          {
+            fromBlock: bridge.ethBlock,
+            toBlock: 'latest',
+          },
+          async (err, events) => {
+            if (events && events.length != 0) {
+              events.forEach((event) => {
+                event.sourceChain = ETH_NETWORK;
+              });
+              allEvents = allEvents.concat(events);
+              ethNewBlock = parseInt(events[0].blockNumber) + 1;
+            } else {
+              ethNewBlock = await web3.eth.getBlockNumber();
+            }
+          },
+        );
+      } else {
+        ethNewBlock = await web3.eth.getBlockNumber();
+      }
     }
 
     if (BEP_BRIDGE && TOKEN_BEP) {
-      var web3 = new Web3(BSC_URL);
-      var contract = new web3.eth.Contract(BRIDGE_ABI, BEP_BRIDGE);
-      await contract.getPastEvents(
-        'Payback',
-        {
-          fromBlock: bridge.bnbBlock,
-          toBlock: 'latest',
-        },
-        async (err, events) => {
-          if (events && events.length != 0) {
-            events.forEach((event) => {
-              event.sourceChain = BSC_NETWORK;
-            });
-            allEvents = allEvents.concat(events);
-            bnbNewBlock = parseInt(events[0].blockNumber) + 1;
-          }
-        },
-      );
+      let web3 = new Web3(BSC_URL);
+
+      if (bridge.bnbBlock) {
+        let contract = new web3.eth.Contract(BRIDGE_ABI, BEP_BRIDGE);
+        await contract.getPastEvents(
+          'Payback',
+          {
+            fromBlock: bridge.bnbBlock,
+            toBlock: 'latest',
+          },
+          async (err, events) => {
+            if (events && events.length != 0) {
+              events.forEach((event) => {
+                event.sourceChain = BSC_NETWORK;
+              });
+              allEvents = allEvents.concat(events);
+              bnbNewBlock = parseInt(events[0].blockNumber) + 1;
+            } else {
+              bnbNewBlock = await web3.eth.getBlockNumber();
+            }
+          },
+        );
+      } else {
+        bnbNewBlock = await web3.eth.getBlockNumber();
+      }
     }
 
     if (MAT_BRIDGE && TOKEN_MAT) {
-      var web3 = new Web3(MAT_URL);
-      var contract = new web3.eth.Contract(BRIDGE_ABI, MAT_BRIDGE);
-      await contract.getPastEvents(
-        'Payback',
-        {
-          fromBlock: bridge.matBlock,
-          toBlock: 'latest',
-        },
-        async (err, events) => {
-          if (events && events.length != 0) {
-            events.forEach((event) => {
-              event.sourceChain = MAT_NETWORK;
-            });
-            allEvents = allEvents.concat(events);
-            matNewBlock = parseInt(events[0].blockNumber) + 1;
-          }
-        },
-      );
+      let web3 = new Web3(MAT_URL);
+
+      if (bridge.matBlock) {
+        let contract = new web3.eth.Contract(BRIDGE_ABI, MAT_BRIDGE);
+        await contract.getPastEvents(
+          'Payback',
+          {
+            fromBlock: bridge.matBlock,
+            toBlock: 'latest',
+          },
+          async (err, events) => {
+            if (events && events.length != 0) {
+              events.forEach((event) => {
+                event.sourceChain = MAT_NETWORK;
+              });
+              allEvents = allEvents.concat(events);
+              matNewBlock = parseInt(events[0].blockNumber) + 1;
+            } else {
+              matNewBlock = await web3.eth.getBlockNumber();
+            }
+          },
+        );
+      } else {
+        matNewBlock = await web3.eth.getBlockNumber();
+      }
     }
   }
   return {
     events: allEvents,
-    ethNewBlock: ethNewBlock ? ethNewBlock : bridge.ethBlock,
-    bnbNewBlock: bnbNewBlock ? bnbNewBlock : bridge.bnbBlock,
-    matNewBlock: matNewBlock ? matNewBlock : bridge.matBlock,
+    ethNewBlock: ethNewBlock,
+    bnbNewBlock: bnbNewBlock,
+    matNewBlock: matNewBlock,
   };
 };
